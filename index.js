6728dbde221f0307942ec06f01603c92748196f5
Kenya Foster <kjewells@Kenyas-MacBook-Pro.local>
2022-09-24 18:22:16 -0400

    adding elements and functions to splash page

---
+++ index.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('.button')
+console.log(button)
+
+const affirmation = document.querySelector('.affirmation')
+console.log(affirmation)
+
+const enter = document.querySelector('.enter')
+console.log(enter)
+
+button.addEventListener('click' , function () {
+    button.style.opacity = 0
+    affirmation.style.opacity = 1
+    enter.style.opacity = 1
+    console.log(enter)
+}
+
+)
